ce7af4c1e582b32e98fc8197cd1de83e2769b09f
Sarah Shelley <35126840+sargef@users.noreply.github.com>
2020-10-28 20:03:51 +1030

    Update config.php

--- config.php
+++ config.php
@@ -1,18 +1,11 @@
 <?php
 
 define('DB_HOST', 'localhost');
-define('DB_NAME', 'sarahsh2_cms_2');
-define('DB_USER', 'sarahsh2_sarah');
-define('DB_PASS', 'Danmicjos12383');
+define('DB_NAME', '');
+define('DB_USER', '');
+define('DB_PASS', '');
 
+define('EMAIL', '');
 
-define('EMAIL', 'sargef3838@gmail.com');
-
-// define('SMTP_HOST', 'smtp.telstra.com');
-// define('SMTP_USER', 'sargef38@bigpond.com');
-// define('SMTP_PASS', 'Danmicjos12383');
-// define('SMTP_PORT', '465');
-// define('SMTP_SET_FROM', 'sargef38@bigpond.com');
-// define('SMTP_ADD_ADDRESS', 'sargef38@bigpond.com');
 
 define('SHOW_ERROR_DETAIL', false);
